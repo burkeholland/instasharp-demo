f65ecc4620048d76c61754f42b3a123e12dae3ff
Burke Holland <burkeholland@gmail.com>
2013-07-24 11:29:13 -0500

    Add scenario for private accounts

--- Scripts/app/mylibs/users/users.coffee
+++ Scripts/app/mylibs/users/users.coffee
@@ -10,15 +10,29 @@
     userid = null
     detailTemplate = kendo.template detailTemplate
     el = null
+    userEl = null
+
+    state = 
+        loading: (isLoading, denied) ->
+            kendo.ui.progress $("body"), isLoading
+            viewModel.set "loading", isLoading
+            if denied != undefined
+                viewModel.set "denied", denied
 
     user = {
         get: () -> 
+            
+            state.loading true
+            
             $.get "api/users/#{userid}", (data) ->
-                # clear the container
-                el.empty()
-                el.append(detailTemplate(data.data))
+                if data.meta.code == 400
+                    state.loading false, true
+                else
+                    # clear the container
+                    userEl.empty()
+                    userEl.append(detailTemplate(data.data))
                 
-                viewModel.get("data.recent").read()
+                    viewModel.get("data.recent").read()
     }
 
     viewModel = kendo.observable({
@@ -30,11 +44,17 @@
                             "api/users/#{userid}/recent"
                     parameterMap: ->
                         count: 40
+                change: ->
+                    state.loading false, false
                 schema:
                     data: "data"
             }
-        user: 
-            name: ""
+        loading: true
+        denied: false
+        user: ->
+            if @get("loading") == false and not @get("denied") == true then true else false
+        error: ->
+            if @get("loading") == false and @get("denied") == true then true else false
     })
 
     if $("#users-template").length == 0 then $("body").append usersTemplate
@@ -47,7 +67,8 @@
             view = new kendo.View template, {
                 model: viewModel,
                 init: ->
-                    el = this.element.find "#user"
+                    el = this.element
+                    userEl = this.element.find "#user"
             }
         else
             return view    
--- Scripts/app/mylibs/users/users.js
+++ Scripts/app/mylibs/users/users.js
@@ -1,18 +1,33 @@
 ﻿(function() {
 
   define(['jquery', 'kendo', 'text!mylibs/users/views/users-template.html', 'text!mylibs/users/views/user-details.html', 'text!mylibs/users/views/users.html'], function($, kendo, usersTemplate, detailTemplate, template) {
-    var el, user, userid, view, viewModel,
+    var el, state, user, userEl, userid, view, viewModel,
       _this = this;
     view = null;
     userid = null;
     detailTemplate = kendo.template(detailTemplate);
     el = null;
+    userEl = null;
+    state = {
+      loading: function(isLoading, denied) {
+        kendo.ui.progress($("body"), isLoading);
+        viewModel.set("loading", isLoading);
+        if (denied !== void 0) {
+          return viewModel.set("denied", denied);
+        }
+      }
+    };
     user = {
       get: function() {
+        state.loading(true);
         return $.get("api/users/" + userid, function(data) {
-          el.empty();
-          el.append(detailTemplate(data.data));
-          return viewModel.get("data.recent").read();
+          if (data.meta.code === 400) {
+            return state.loading(false, true);
+          } else {
+            userEl.empty();
+            userEl.append(detailTemplate(data.data));
+            return viewModel.get("data.recent").read();
+          }
         });
       }
     };
@@ -31,13 +46,29 @@
               };
             }
           },
+          change: function() {
+            return state.loading(false, false);
+          },
           schema: {
             data: "data"
           }
         })
       },
-      user: {
-        name: ""
+      loading: true,
+      denied: false,
+      user: function() {
+        if (this.get("loading") === false && !this.get("denied") === true) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      error: function() {
+        if (this.get("loading") === false && this.get("denied") === true) {
+          return true;
+        } else {
+          return false;
+        }
       }
     });
     if ($("#users-template").length === 0) {
@@ -49,7 +80,8 @@
           return view = new kendo.View(template, {
             model: viewModel,
             init: function() {
-              return el = this.element.find("#user");
+              el = this.element;
+              return userEl = this.element.find("#user");
             }
           });
         } else {
