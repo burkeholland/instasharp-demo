b192c55533ef13344f0254a91b047ce73d28ef7e
Burke Holland <burkeholland@gmail.com>
2013-07-24 11:30:22 -0500

    Add Kendo UI Q2 Release

---
+++ Scripts/test.js
@@ -0,0 +1,15 @@
+﻿// you included jQuery, but never used it
+(function ($) {
+
+    // pub is defined but never used
+    var pub = {
+        // this is part of an object literal, not an assignment
+        init: function(response) {
+            // respnse should be response
+            console.log(respnse);
+        }
+    }
+
+    // you're missing a semicolon here
+    // also - its jQuery, not jquery
+}(jquery))
